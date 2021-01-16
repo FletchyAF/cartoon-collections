6c11fa09aff491fd9b2406925cbe28f7bb596274
FletchyAF <joshua.f1998@gmail.com>
2021-01-16 17:04:59 -0500

    commit tree 65902625048410d5639f52a48c3684435cc2c3bc

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  number = 1
+  array.each do |dwarf|
+    puts "#{number}. #{dwarf}" 
+    number += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = []
+  new_array = array.collect { |element| "#{element.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |element| element.length >= 5 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
